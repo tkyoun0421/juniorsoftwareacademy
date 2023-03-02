fd8d1e913dde357a137f90e9ede0d1507cb9283e
Taegwan Youn <tkyoun0421@naver.com>
2023-03-03 02:35:49 +0900

    circle-box 완성

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
 $(function(){
-  
-  // nav hover시 gnb show 기능
-  $(".nav").mouseenter(showGnb);
-
-  $(".nav").mouseleave(removeGnb);
-
-  $(".gnb a").focus(showGnb);
 
   // 뷰포트 반응형
   $(window).resize(function(){
@@ -15,14 +8,15 @@ $(function(){
       $(".spot-menu a span").eq(0).removeClass("blind")
     }
   });
-
+  
+  // gnb 보여주기 기능
   function showGnb() {
     $(".header").addClass("show");
     $(".header .logo img").odd().removeClass("hidden");
     $(".header .logo img").even().addClass("hidden");
     $(".header .spot-menu a").eq(1).css({'display': 'none'});
     $(".header .spot-menu a").eq(2).css({'display': 'block'});
-  }
+  };
 
   function removeGnb() {
     $(".header").removeClass("show");
@@ -30,5 +24,13 @@ $(function(){
     $(".header .logo img").odd().addClass("hidden");
     $(".header .spot-menu a").eq(1).css({'display': 'block'});
     $(".header .spot-menu a").eq(2).css({'display': 'none'});
-  }
+  };
+
+  $(".nav").mouseenter(showGnb);
+
+  $(".nav").mouseleave(removeGnb);
+
+  $(".gnb a").focus(showGnb);
+  $(".spot-menu a").focus(removeGnb);
+
 });
