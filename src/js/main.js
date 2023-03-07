0471c702f10583e5ea9d5326c61f775cb016eebc
Taegwan Youn <tkyoun0421@naver.com>
2023-03-07 17:21:13 +0900

    make banner-sec 완성

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 $(function(){
 
   var $winWidth = $(window).width();
+  let visibleScreen =  0;
   const TABLET_WIDTH = 1200;
   const MOBILE_WIDTH = 768;
 
@@ -15,8 +16,16 @@ $(function(){
     if($winWidth > TABLET_WIDTH) {
       closeBars();
     }
-    
+
   });
+
+  function addClassScroll() {
+    $(".header").addClass("scroll");
+  }
+
+  function removeClassScroll() {
+    $(".header").removeClass("scroll");
+  }
   
   function showGnb() {
     if ($winWidth > TABLET_WIDTH) {
@@ -46,9 +55,19 @@ $(function(){
   function scrollFixedHeader() {
     $(window).scroll(function() {
       if($(window).scrollTop() > 1) {
-        $(".header").addClass("scroll");
+        addClassScroll();
       } else {
-        $(".header").removeClass("scroll");
+        removeClassScroll();
+      }
+    });
+  }
+
+  function openDoor() {
+    $(window).scroll(function(){
+      visibleScreen = $(window).scrollTop() + $(window).height() - ($(window).height() / 3);
+      if(visibleScreen > $(".banner-sec").offset().top){
+        $(".banner-sec").addClass("on");
+        console.log("@");
       }
     });
   }
@@ -56,6 +75,7 @@ $(function(){
   // 실행
   function init() {
     scrollFixedHeader();
+    openDoor();
   }
 
   function event() {
@@ -65,7 +85,11 @@ $(function(){
     $(".depth-bg").mouseleave(removeGnb);
     $(".gnb").mouseleave(removeGnb);
     $(".gnb a").focus(showGnb);
+    $(".header a").focus(addClassScroll);
+    $(".header").blur(removeClassScroll);
     $(".spot-menu button").focus(removeGnb);    
+    $(".spot-menu").focus(addClassScroll);    
+    $(".spot-menu").blur(removeClassScroll);    
     $(".close").on("click", closeBars);
     $(".depth1").on("click", openDepth2);
   };
