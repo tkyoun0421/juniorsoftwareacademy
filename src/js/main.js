9cebdf95fce89290f475f0f35b5dd6fd2217feb8
Taegwan Youn <tkyoun0421@naver.com>
2023-03-07 15:34:48 +0900

    make program sec 완성

--- src/js/main.js
+++ src/js/main.js
@@ -9,81 +9,28 @@ $(function(){
 
     if($winWidth < TABLET_WIDTH) {
       $(".spot-menu .login span").addClass("blind");
-      scrollLogoChange();      
     } else {
       $(".spot-menu .login span").removeClass("blind");
     }
     if($winWidth > TABLET_WIDTH) {
       closeBars();
     }
-
-    if($winWidth > MOBILE_WIDTH) {
-      if ($(".header").hasClass("show")) {
-        blackKakao()  
-      } else {
-        whiteKakao()
-      }
-    } else {
-      $(".header .spot-menu .kakao").css({'display': 'none'});
-    }
+    
   });
   
   function showGnb() {
     if ($winWidth > TABLET_WIDTH) {
-      $(".header").addClass("show");      
-      $(".header").removeClass("scroll");
-      changeColorKakao();
+      $(".header").addClass("show");     
     }
-    toggleLogoColor();
   };
 
   function removeGnb() {
     if ($winWidth > TABLET_WIDTH) {
       $(".header").removeClass("show");      
-      changeColorKakao();
     }
-    toggleLogoColor();
   };
 
-  function toggleLogoColor() {
-    if ($(".header").hasClass("show")) {
-      $(".logo img").removeClass("hidden");
-      $(".logo img").even().addClass("hidden");
-    } else {
-      $(".logo img").removeClass("hidden");
-      $(".logo img").odd().addClass("hidden");
-    }    
-  }
-
-  function scrollLogoChange() {
-    if ($(".header").hasClass("scroll")) {
-      $(".logo img").removeClass("hidden");
-      $(".logo img").even().addClass("hidden");
-    } else {
-      $(".logo img").removeClass("hidden");
-      $(".logo img").odd().addClass("hidden");
-    }
-  }
-
-  function changeColorKakao() {
-    if ($(".header").hasClass("show")) {
-      blackKakao();
-    } else {
-      whiteKakao();
-    }
-  }
-
-  function blackKakao() {
-    $(".header .spot-menu .kakao").eq(0).css({'display': 'none'});
-    $(".header .spot-menu .kakao").eq(1).css({'display': 'block'});
-  }
-
-  function whiteKakao() {
-    $(".header .spot-menu .kakao").eq(0).css({'display': 'block'});
-    $(".header .spot-menu .kakao").eq(1).css({'display': 'none'});
-  }
-
-  function onBar (){
+  function onBars (){
     $(".nav").addClass("on");
   }
   
@@ -100,12 +47,8 @@ $(function(){
     $(window).scroll(function() {
       if($(window).scrollTop() > 1) {
         $(".header").addClass("scroll");
-        scrollLogoChange();       
-        blackKakao(); 
       } else {
         $(".header").removeClass("scroll");
-        scrollLogoChange();
-        whiteKakao();
       }
     });
   }
@@ -116,13 +59,13 @@ $(function(){
   }
 
   function event() {
-    $(".bars").on("click", onBar);
+    $(".bars").on("click", onBars);
     $(".gnb").mouseenter(showGnb);
     $(".depth-bg").mouseenter(showGnb);
     $(".depth-bg").mouseleave(removeGnb);
     $(".gnb").mouseleave(removeGnb);
     $(".gnb a").focus(showGnb);
-    $(".spot-menu button").focus(removeGnb);
+    $(".spot-menu button").focus(removeGnb);    
     $(".close").on("click", closeBars);
     $(".depth1").on("click", openDepth2);
   };
