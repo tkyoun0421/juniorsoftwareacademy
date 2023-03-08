4dfc118453523cbc6d8f137446146e53c95d25f9
Taegwan Youn <tkyoun0421@naver.com>
2023-03-08 17:04:15 +0900

    finish index.html 완성

--- src/js/main.js
+++ src/js/main.js
@@ -5,25 +5,48 @@ $(function(){
   const TABLET_WIDTH = 1200;
   const MOBILE_WIDTH = 768;
 
-  $(window).resize(function(){
-    $winWidth = $(window).width();
+  // 반응형 이벤트
+  function resizeEvent() {
+    $(window).resize(function(){
+      $winWidth = $(window).width();
+      if ($winWidth < TABLET_WIDTH) {
+        addClassLoginBlind();
+      } else {
+        removeClassLoginBlind();
+        closeBars();
+      }
+    });
+  }
 
-    if($winWidth < TABLET_WIDTH) {
-      $(".spot-menu .login span").addClass("blind");
-    } else {
-      $(".spot-menu .login span").removeClass("blind");
-    }
-    if($winWidth > TABLET_WIDTH) {
-      closeBars();
-    }
+  // 스크롤 이벤트
+  function scrollEvent(){
+    $(window).scroll(function(){
+      openDoor();
+      scrollFixedHeader();
+    });
+  };
 
-  });
+  // 새로고침 감지 이벤트
+  function refreshEvent() {
+    $(window).on('load', function(){
+      openDoor();
+      scrollFixedHeader();
+    });
+  }
 
-  function addClassScroll() {
+  function addClassLoginBlind() {
+    $(".spot-menu .login span").addClass("blind");
+  }
+
+  function removeClassLoginBlind() {
+    $(".spot-menu .login span").removeClass("blind");
+  }
+
+  function addClassHeaderScroll() {
     $(".header").addClass("scroll");
   }
 
-  function removeClassScroll() {
+  function removeClassHeaderScroll() {
     $(".header").removeClass("scroll");
   }
   
@@ -53,31 +76,57 @@ $(function(){
   }
 
   function scrollFixedHeader() {
-    $(window).scroll(function() {
-      if($(window).scrollTop() > 1) {
-        addClassScroll();
-      } else {
-        removeClassScroll();
-      }
+    if($(window).scrollTop() > 1) {
+      addClassHeaderScroll();
+    } else {
+      removeClassHeaderScroll();
+    }
+  }  
+
+  function openDoor() {
+    visibleScreen = $(window).scrollTop() + $(window).height() - ($(window).height() / 3);
+    if(visibleScreen > $(".banner-sec").offset().top){
+      $(".banner-sec").addClass("on");
+    };
+  }
+
+  function addClassTabOn() {
+    $(".tab li").on("click", function(){
+      $(".tab li").removeClass("on");
+      $(this).addClass("on");
     });
   }
 
-  function openDoor() {
-    $(window).scroll(function(){
-      visibleScreen = $(window).scrollTop() + $(window).height() - ($(window).height() / 3);
-      if(visibleScreen > $(".banner-sec").offset().top){
-        $(".banner-sec").addClass("on");
-        console.log("@");
-      }
+  function addClassPointerOn() {
+    $(".now-sec .img-box").on("mouseenter", function(){
+      $(".cursor").addClass("on");
     });
+    $(".now-sec .img-box").on("mouseleave", function(){
+      $(".cursor").removeClass("on");
+    });
+  }
+
+  function movePointer(e) {
+    if ($(".cursor").hasClass("on")) {
+      $(".cursor").css({
+        "left" : e.pageX,
+        "top" : e.pageY
+      });
+    }
   }
   
   // 실행
   function init() {
     scrollFixedHeader();
     openDoor();
+    refreshEvent();
+    scrollEvent();
+    resizeEvent();
+    addClassTabOn();
+    addClassPointerOn();
   }
 
+  // 이벤트
   function event() {
     $(".bars").on("click", onBars);
     $(".gnb").mouseenter(showGnb);
@@ -85,13 +134,14 @@ $(function(){
     $(".depth-bg").mouseleave(removeGnb);
     $(".gnb").mouseleave(removeGnb);
     $(".gnb a").focus(showGnb);
-    $(".header a").focus(addClassScroll);
-    $(".header").blur(removeClassScroll);
+    $(".header a").focus(addClassHeaderScroll);
+    $(".header").blur(removeClassHeaderScroll);
     $(".spot-menu button").focus(removeGnb);    
-    $(".spot-menu").focus(addClassScroll);    
-    $(".spot-menu").blur(removeClassScroll);    
+    $(".spot-menu").focus(addClassHeaderScroll);    
+    $(".spot-menu").blur(removeClassHeaderScroll);    
     $(".close").on("click", closeBars);
     $(".depth1").on("click", openDepth2);
+    $(window).on("mousemove", movePointer);
   };
 
   init();
